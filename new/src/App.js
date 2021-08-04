ad47c6286f2784ed2437bafca0084879d46fb3c3
Nada0795 <nadakh1993@icloud.com>
2021-08-04 14:16:03 +0300

    add note

---
+++ new/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>city explorer</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
+
