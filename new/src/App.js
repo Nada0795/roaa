24241cca1772103ecfb535b42c2522a57c83ec94
Nada0795 <nadakh1993@icloud.com>
2021-08-04 21:30:42 +0300

    resolve lab1-week2

--- new/src/App.js
+++ new/src/App.js
@@ -1,14 +1,63 @@
 import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component{
+
+constructor (props){
+super(props);
+this.state = {
+displayName : '',
+lat : '',
+lon : '',
+showMap : false
+}
+}
+
+getData=async(event)=>{
+event.preventDefault();
+
+let cityName=event.target.city.value;
+console.log(cityName);
 
 
+let URL= `https://eu1.locationiq.com/v1/search.php?key=pk.e98623dff1c928d4100d271b111bad0d&q=${cityName}&format=json`;
 
- class App extends Component {
-  render() {
-    return (
-      <div>
+console.log(URL);
+
+let getResult=await axios.get(URL);
+// console.log(getResult.data[0].display_name);
+
+this.setState ({
+displayName : getResult.data[0].display_name,
+lat : getResult.data[0].lat,
+lon : getResult.data[0].lot,
+showMap : true
+})
+}
+
+
+render() {
+  return (
+      <>
+
+
       <h1>city explorer</h1>
-      </div>
-    )
+
+      <form onSubmit={this.getData}>
+  <input type='text' placeholder='enter city' name='city'/>
+
+   <button type='submit'>Get Location</button>
+
+      </form>
+
+
+<p> {this.state.displayName}</p> 
+
+{this.state.showMap&& <img src= {`https://maps.locationiq.com/v3/staticmap?key=pk.e98623dff1c928d4100d271b111bad0d&q&center=${this.state.lat},${this.state.lon}`} alt = 'map' />}
+  </>
+
+  )
+
   }
 }
 
